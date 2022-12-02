334cd3f64cdd87a5c345272a8b73d362221d3b1d
Ji Yongfeng <aqmails@gmail.com>
2022-12-02 01:53:41 +0000

    hello world in 12-02

--- streamlitdemo.py
+++ streamlitdemo.py
@@ -7,13 +7,16 @@
    * @LastEditTime: 2022-09-18 11:38:07
    * @LastEditor: Ji Yongfeng
    * @Description: streamlit demo 111
- '''
+'''
+
 import streamlit as st
 import pandas as pd
 import numpy as np
 import os
 import altair as alt
 
-df = pd.DataFrame(np.random.randn(200, 3),columns=['a', 'b', 'c'])
-c = alt.Chart(df).mark_circle().encode(x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
+df = pd.DataFrame(np.random.randn(200, 3), columns=['a', 'b', 'c'])
+c = alt.Chart(df).mark_circle().encode(
+    x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
 st.write(c)
+print("hello world")
